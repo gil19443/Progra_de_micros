ca7b083af269513bd026ca62a2f6aaf25582df36
gil19443 <gil19443@uvg.edu.gt>
2020-10-15 22:13:35 -0600

    muxeo de lectura ADC  y envio de datos

--- SERIAL.py
+++ SERIAL.py
@@ -6,20 +6,24 @@ import sys
 #COMUNICACION SERIAL DE PIC CON LA CUMPU
 
 # Configurar el puerto serial. Elejir el puerto en el que aparece en su computadora y la velocidad
-ser= serial.Serial(port='COM5',baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS, timeout=0)
 
 # loop infinito
 while 1:
-    #borrar el buffer para iniciar en cero
-    ser.flushInput()
-    ser.flushOutput()
-    #esperar un tiempo para recibir datos
-    time.sleep(.3)
-    #leer dato serial
-    recibido1=ser.read() #readline lee hasta encontrar el enter ASCII "A"h
-    #escribir dato serial
-    ser.write(recibido1)
-    #convertir a número de 8 bits e imprimir el dato recibido
-    numero = ord(recibido1)
-    print(numero)
-    # RECUERDEN CONECTAR EL RX del pic AL TX de la compu
+        ser = serial.Serial(port='COM5',baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS, timeout=0)
+        #borrar el buffer para iniciar en cero
+        ser.flushInput()
+        ser.flushOutput()
+        #esperar un tiempo para recibir datos
+        time.sleep(.3)
+        #leer dato serial
+        #x=ser.read() #readline lee hasta encontrar el enter ASCII "A"h
+        #esperar un tiempo para recibir datos
+        #time.sleep(.3)
+        y=ser.read() #readline lee hasta encontrar el enter ASCII "A"h
+        #escribir dato serial
+        #ser.write(0x0A)
+        #convertir a número de 8 bits e imprimir el dato recibido#x_final = ord(y)
+        #y_final = ord(y)
+        print(y)
+        ser.close()
+        # RECUERDEN CONECTAR EL RX del pic AL TX de la compu
